994d231db5c146d45f91ed8a6ca7e92f71227829
halulu <lzjluzijie@gmail.com>
2020-03-23 15:15:22 -0400

    脚本并手动整理 2003 元老院秘闻-高阳同志不愿提及的往事

--- data/t.py
+++ data/t.py
@@ -1,6 +1,6 @@
 from lxml import etree
 
-f = open('2002.html', 'r', encoding='UTF-8')
+f = open('2003.html', 'r', encoding='UTF-8')
 s = etree.HTML(f.read())
 
 s = s[0][0]
@@ -15,12 +15,12 @@ s = s[0][0]
 #     else:
 #         inner(element[0])
 
-t = '---\naid: 2002\nzid: %d\ntitle: 2002.%04d-%s\nauthor: 社会主义螺丝刀\n---\n\n'
+t = '---\naid: 2003\nzid: %d\ntitle: 2003.%04d-%s\nauthor: 社会主义螺丝刀\n---\n\n'
 
 zid = 1
 for n in s:
     if n.tag == "h2":
-        f = open("output/2002/%04d.md" % zid, "w", encoding='UTF-8')
+        f = open("output/2003/%04d.md" % zid, "w", encoding='UTF-8')
         f.write(t % (zid, zid, n[0].text))
         zid = zid + 1
     else:
