57dadda580ae8a8863787d222ac620e54ff47020
halulu <lzjluzijie@gmail.com>
2020-03-23 00:28:00 -0400

    学习python，感谢xqm32

---
+++ data/t.py
@@ -0,0 +1,29 @@
+from lxml import etree
+
+f = open('2002.html', 'r', encoding='UTF-8')
+s = etree.HTML(f.read())
+
+s = s[0][0]
+
+# print(etree.tostring(s[0][0], pretty_print=True))
+# print(s[0][0].text)
+
+
+# def inner(element):
+#     if element.text != "":
+#         return element.text
+#     else:
+#         inner(element[0])
+
+t = '---\naid: 2002\nzid: %d\ntitle: 2002.%04d-%s\n---\n\n'
+
+zid = 1
+for n in s:
+    if n.tag == "h2":
+        f = open("output/2002/%04d.md" % zid, "w", encoding='UTF-8')
+        f.write(t % (zid, zid, n[0].text))
+        zid = zid + 1
+    else:
+        if n.text is not None:
+            f.write(n.text)
+            f.write("\n")
