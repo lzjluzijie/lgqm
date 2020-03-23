655ff5b7008ad062159709d72e3216700e6192d8
halulu <lzjluzijie@gmail.com>
2020-03-23 15:25:16 -0400

    脚本并手动整理 2004 外事无小事—第二季大明狼烟

--- data/t.py
+++ data/t.py
@@ -1,29 +1,24 @@
 from lxml import etree
 
-f = open('2003.html', 'r', encoding='UTF-8')
-s = etree.HTML(f.read())
 
-s = s[0][0]
+def wiki2md(aid, author):
+    f = open(aid + '.html', 'r', encoding='UTF-8')
+    s = etree.HTML(f.read())
 
-# print(etree.tostring(s[0][0], pretty_print=True))
-# print(s[0][0].text)
+    s = s[0][0]
+
+    t = '---\naid: ' + aid + '\nzid: %d\ntitle: ' + aid + '.%04d-%s\nauthor: ' + author + '\n---\n\n'
+
+    zid = 1
+    for n in s:
+        if n.tag == "h2":
+            f = open("output/" + aid + "/%04d.md" % zid, "w", encoding='UTF-8')
+            f.write(t % (zid, zid, n[0].text))
+            zid = zid + 1
+        else:
+            if n.text is not None:
+                f.write(n.text)
+                f.write("\n")
 
 
-# def inner(element):
-#     if element.text != "":
-#         return element.text
-#     else:
-#         inner(element[0])
-
-t = '---\naid: 2003\nzid: %d\ntitle: 2003.%04d-%s\nauthor: 社会主义螺丝刀\n---\n\n'
-
-zid = 1
-for n in s:
-    if n.tag == "h2":
-        f = open("output/2003/%04d.md" % zid, "w", encoding='UTF-8')
-        f.write(t % (zid, zid, n[0].text))
-        zid = zid + 1
-    else:
-        if n.text is not None:
-            f.write(n.text)
-            f.write("\n")
+wiki2md("2004", "社会主义螺丝刀")
