496150244007b8dcb6533494d87832ca68c88754
halulu <lzjluzijie@gmail.com>
2020-04-06 08:57:12 -0400

    测试

--- data/northdy.go
+++ data/northdy.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"encoding/gob"
 	"encoding/json"
 	"fmt"
+	"github.com/lunny/html2md"
 	"io"
 	"os"
+	"text/template"
 	"time"
 )
 
@@ -16,6 +16,11 @@ import (
 数据来源 https://lgqm.gq/thread-1722-1-1.html
 */
 
+type User struct {
+	Uid int
+	Name string
+}
+
 type Post struct {
 	Pid      int
 	Uid      int
@@ -23,6 +28,7 @@ type Post struct {
 
 	PostTimeString string `json:"post_time"`
 
+	Username string
 	PostTime time.Time
 }
 
@@ -35,6 +41,7 @@ type Thread struct {
 	PostTimeString string `json:"post_time"`
 	LastPostTimeString string `json:"last_post_time"`
 
+	Username string
 	PostTime     time.Time
 	LastPostTime time.Time
 	Posts        []*Post
@@ -42,8 +49,84 @@ type Thread struct {
 
 const PostTimeLayout = "2006-1-2 15:04:05"
 const LastPostTimeLayout = "2006-1-2 15:04"
+const TimeLayout = "2006-01-02 15:04"
 
-func jsonl()  {
+const Northdy = `---
+aid: 9025
+zid: {{ .Tid }}
+title: {{ .Title }}
+author: {{ .Author }}
+---
+
+{{ range .Posts }}
+
+{{ .Username }} 于 {{ .PostTimeString }} 发表评论：
+
+{{ .Content }}
+
+---------
+{{ end }}
+
+`
+
+var tc *template.Template
+
+func init() {
+	t, err := template.New("northdy").Parse(Northdy)
+	if err != nil {
+		panic(err)
+	}
+
+	tc = t
+}
+
+func exec(thread *Thread) {
+	f, err := os.Create(fmt.Sprintf("./output/9025/%06d.md", thread.Tid))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(thread.Title)
+	err = tc.Execute(f, thread)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadUsers() (users map[int]string) {
+	f, err := os.Open("user.jsonl")
+	if err != nil {
+		panic(err)
+	}
+
+	r := bufio.NewReader(f)
+	users = make(map[int]string)
+
+	for {
+		//ReadLine 有问题
+		l, err := r.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		var user User
+		err = json.Unmarshal([]byte(l), &user)
+		if err != nil {
+			panic(err)
+		}
+
+		users[user.Uid] = user.Name
+	}
+
+	return
+}
+
+func jsonl() (threads []*Thread)  {
+	users := loadUsers()
+
 	beijing := time.FixedZone("北京时间", int((8 * time.Hour).Seconds()))
 
 	f, err := os.Open("thread.jsonl")
@@ -52,7 +135,7 @@ func jsonl()  {
 	}
 
 	r := bufio.NewReader(f)
-	threads := make([]*Thread, 0)
+	threads = make([]*Thread, 0)
 
 	for {
 		//ReadLine 有问题
@@ -92,6 +175,8 @@ func jsonl()  {
 		}
 		t.LastPostTime = lpt
 
+		t.Username = users[t.Author]
+
 		for _, pts := range t.Posts {
 			pt, err := time.ParseInLocation(PostTimeLayout, pts.PostTimeString, beijing)
 			if err != nil {
@@ -99,24 +184,26 @@ func jsonl()  {
 			}
 
 			pts.PostTime = pt
+			pts.Username = users[t.Author]
+			if t.Tid == 787250 {
+				pts.Content = html2md.Convert(pts.Content)
+			}
 		}
 
-		fmt.Println(t.Title)
-		fmt.Println(t.PostTime)
+		//fmt.Println(t.Tid)
+		//fmt.Println(t.Title)
+		//fmt.Println(t.PostTime)
 
 		threads = append(threads, t)
 	}
 
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	err = enc.Encode(threads)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(buf.Len())
+	return
 }
 
 func main() {
-	jsonl()
+	//fmt.Println(loadUsers())
+	threads := jsonl()
+	fmt.Println(threads[0].Posts[0].Content)
+	fmt.Println(html2md.Convert(threads[0].Posts[0].Content))
+	exec(threads[0])
 }
