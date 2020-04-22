d24acc02bd6675a4fcf484bfde0333ff28c564d8
halulu <lzjluzijie@gmail.com>
2020-04-22 12:52:07 -0400

    目录修改

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -4,10 +4,29 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const List = ({node, length}) => (
+  <tr>
+  <td>{node.frontmatter.aid.toString().padStart(4, `0`)}</td>
+  <td>
+    <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+  </td>
+  <td>{node.frontmatter.author}</td>
+  <td>{length}</td>
+  <td>todo</td>
+</tr>
+)
+
+const Single = ({node}) => (
+  <tr>
+    
+  </tr>
+)
+
 const Archive = ({ data }) => (
   <Layout>
     <SEO title="目录" />
     <h1 className="title has-text-centered">目录</h1>
+    <h2 class="subtitle">以下是已收录分卷列表，共 {data.allMarkdownRemark.group.length} 卷</h2>
     <table class="table is-fullwidth">
       <thead>
         <tr>
@@ -19,16 +38,25 @@ const Archive = ({ data }) => (
         </tr>
       </thead>
       <tbody>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <tr>
-            <td>{node.frontmatter.aid.toString().padStart(4, `0`)}</td>
-            <td>
-              <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
-            </td>
-            <td>{node.frontmatter.author}</td>
-            <td>todo</td>
-            <td>todo</td>
-          </tr>
+        {data.allMarkdownRemark.group.map(({ nodes }) =>(
+          <List node={nodes[nodes.length-1]} length={nodes.length-1}></List>
+        ))}
+      </tbody>
+    </table>
+    <h2 class="subtitle">以下是已收录分卷列表，共 {data.allMarkdownRemark.group.length} 卷</h2>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>编号</th>
+          <th>标题</th>
+          <th>作者</th>
+          <th>文章数量</th>
+          <th>上次修改</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.allMarkdownRemark.group.map(({ nodes }) =>(
+          <List node={nodes[nodes.length-1]} length={nodes.length-1}></List>
         ))}
       </tbody>
     </table>
@@ -39,22 +67,22 @@ export default Archive
 
 export const query = graphql`
   query {
-    allMarkdownRemark(
-      sort: { fields: frontmatter___aid }
-      filter: { fields: { type: { eq: "list" } } }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-          }
+    allMarkdownRemark(sort: {fields: [frontmatter___aid, frontmatter___zid]}) {
+      group(field: frontmatter___aid) {
+        nodes {
           frontmatter {
-            aid
             title
+            aid
+            zid
             author
           }
+          fields {
+            type
+            slug
+          }
         }
+        totalCount
       }
     }
-  }
+  }  
 `
