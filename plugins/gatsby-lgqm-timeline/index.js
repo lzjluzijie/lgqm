68cae094a79d444946524879de0a77d0e0885704
halulu <lzjluzijie@gmail.com>
2020-04-29 18:28:06 -0400

    更新README

--- plugins/gatsby-lgqm-timeline/index.js
+++ plugins/gatsby-lgqm-timeline/index.js
@@ -1,6 +1,7 @@
 module.exports = (node, pluginOptions) => {
   if (node.sourceInstanceName !== `data`) return
-  console.log(node)
+  console.log(`data`)
+  console.log(node.base)
 
   return node.markdownAST
 }
