b2f8ea125088b9448e85b2994f44bb900fe7be21
Halulu <lzjluzijie@gmail.com>
2024-12-01 15:45:06 -0800

    更新群信息

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,10 @@ export const getStaticProps = async ({ params }) => {
 }
 
 const content = `
-临高启明公开图书馆为爱好者建立的开源项目，目的是为临高启明读者提供修订过早期文本以及在线讨论环境。本站非官方，无版权，所有资料为互联网公开资料收集，请勿用于商业用途！如有侵权，请联系我们删除。
+临高启明公开图书馆为爱好者建立的开源项目，目的是为临高启明读者提供修订过早期文本以及在线讨论环境。本站非官方，无版权，所有资料为论坛公开资料收集，请勿用于商业用途！如有侵权，请联系我们删除。
+
+- Discord群：https://discord.gg/XgE4xJM
+- 电报群：https://t.me/lingaoqiming
 `
 
 export default function Home({ data }) {
